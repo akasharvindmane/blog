2f56635d6e8ae0a692de969791ce06c0a19fd2a2
akasharvindmane <akasharvindmane@gmail.com>
2020-04-12 00:31:01 +0800

    add CNAME file with modify package

---
+++ build/precache-manifest.e87dcd5d7e94bbc57fcef81d6e4a4e68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec72b45771fd7f9ff75cbc9f109b13c",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "623de9186c748368d686",
+    "url": "/blog/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ca4e2449ab3c74cae1ce",
+    "url": "/blog/static/js/2.e6c48029.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog/static/js/2.e6c48029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "623de9186c748368d686",
+    "url": "/blog/static/js/main.69551dd9.chunk.js"
+  },
+  {
+    "revision": "e9e1165a57ea3f085cb9",
+    "url": "/blog/static/js/runtime-main.ba8eee8c.js"
+  }
+]);
